3ba8ee7d16163dc17b272d674d7f0ad00c8e1d15
Lushegtech <97428249+Lushegtech@users.noreply.github.com>
2025-02-23 10:25:04 +0100

    handle the remaining edge cases

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ app.post("/add", async (req, res) => {
     const input = req.body["country"];
     
     const result = await db.query(
-      "SELECT country_code FROM countries WHERE LOWER(country_name) = LOWER($1)",
-      [input]
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || LOWER($1) || '%';",
+      [input.toLowerCase()]
     );
 
     if (result.rows.length !== 0) {
